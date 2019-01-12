50d1ff7d1899b3f348a83a77e0b8204561cb746e
MisterAfterlife <35574078+MisterAfterlife@users.noreply.github.com>
2019-01-11 21:38:07 -0500

    Update comfy.js

--- comfy.js
+++ comfy.js
@@ -4,7 +4,8 @@ var links = [ "https://imgur.com/Q3tAUWN",
 				"https://imgur.com/NVjMypF",
 				"https://imgur.com/82i3r8g",
 				"https://i.imgur.com/ZzqR0T3.jpg",
-				"https://imgur.com/UzLB5LT"			
+				"https://imgur.com/UzLB5LT",
+	     			"https://i.imgur.com/LlzmQ1r.jpg"
 				 ]
 				  	  
 				  
